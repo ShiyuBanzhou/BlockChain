6e00b2464efd9984ee6c892b1003dbf48194d072
Kaworu <129658573+qrn12580@users.noreply.github.com>
2025-05-02 12:48:52 +0800

    Update DceBlockchainApplication.java

--- src/main/java/com/bjut/blockchain/DceBlockchainApplication.java
+++ src/main/java/com/bjut/blockchain/DceBlockchainApplication.java
@@ -1,14 +1,21 @@
 package com.bjut.blockchain;
 
+import com.bjut.blockchain.web.service.NodeJoinAndQuit;
+import com.bjut.blockchain.web.util.HttpRequestUtil;
 import com.bjut.blockchain.web.util.IntegrityChecker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DceBlockchainApplication {
 
-  public static void main(String[] args) {
-        //使用jar运行前放开
+    @Autowired
+    private NodeJoinAndQuit nodeJoinAndQuit;
+
+    public static void main(String[] args) {
         if (true || IntegrityChecker.verifyIntegrity()) {
             System.out.println("Code integrity verified. Starting the application...");
             SpringApplication.run(DceBlockchainApplication.class, args);
@@ -17,4 +24,15 @@ public class DceBlockchainApplication {
         }
     }
 
+    @PostConstruct
+    public void init() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                nodeJoinAndQuit.selfQuit();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+    }
 }
+
